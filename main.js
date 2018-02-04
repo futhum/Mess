e98139a504a1846fec6d84dcdc923d3c86573b34
futhum <35200359+futhum@users.noreply.github.com>
2018-02-04 15:56:32 +0300

    Init

---
+++ main.js
@@ -0,0 +1,10 @@
+var app1 = new Vue({
+    el: '#app-1',
+    data: {
+        todos: [
+                {text: '3'},
+                {text: 'Mess2'},
+                {text: 'Mess2'}
+        ]
+    }
+})
